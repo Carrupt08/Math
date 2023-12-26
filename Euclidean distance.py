2b18de19456bea997544762f0d31e7fba47a7c3c
Carrupt08 <140162433+Carrupt08@users.noreply.github.com>
2023-12-26 12:49:04 +0200

    Add files via upload

---
+++ Euclidean distance.py
@@ -0,0 +1,10 @@
+import math
+
+x1 = float(input("x1= "))
+x2 = float(input("x2= "))
+
+y1 = float(input("y1= "))
+y2 = float(input("y2= "))
+d = math.sqrt((y1-x1)**2 + (y2-x2)**2)
+
+print("d= ",d)
